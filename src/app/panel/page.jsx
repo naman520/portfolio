18b1acfbc6c0be0d94612c5e56fbde73fd659be3
bookmyassets <bookmyassets101@gmail.com>
2025-07-21 12:49:47 +0530

    link correction

--- src/app/panel/page.jsx
+++ src/app/panel/page.jsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div>
       <iframe
-        src="https://bigbucket.online/dashboard.php"
+        src="https://bigbucket.online/namanTest/dashboard.php"
         width="100%"
         height="600"
         style={{ border: 'none' }}
