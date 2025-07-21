a2761af0d1f4c2d985f7400af0db0f0223c8824a
bookmyassets <bookmyassets101@gmail.com>
2025-07-21 11:21:36 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/namantest',
-        destination: '/api/namantest', // Proxy to the API route
+        destination: 'https://bma-dholera-sir.onrender.com', // Proxy to the API route
       },
     ];
   },
