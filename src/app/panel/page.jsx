67c27824bce8eece19a47f98aefda60c6403cd8b
bookmyassets <bookmyassets101@gmail.com>
2025-07-21 12:48:14 +0530

    changes

--- src/app/panel/page.jsx
+++ src/app/panel/page.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function page() {
   return (
-    <>
-        <div>
-            <iframe
-  src="https://bigbucket.online/dashboard.php"
-  width="100%"
-  height="600"
-  style="border: none;"
-></iframe>
-
-        </div>
-    </>
-  )
+    <div>
+      <iframe
+        src="https://bigbucket.online/dashboard.php"
+        width="100%"
+        height="600"
+        style={{ border: 'none' }}
+      ></iframe>
+    </div>
+  );
 }
